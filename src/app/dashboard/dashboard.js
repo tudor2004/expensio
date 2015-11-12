d044af5973febba71adca626aa0e11b79d1c3870
Tudor-Dan Ravoiu <tudor2004@yahoo.com>
2015-11-12 18:41:49 +0100

    CHANGE small rework

--- src/app/dashboard/dashboard.js
+++ src/app/dashboard/dashboard.js
@@ -1,17 +1,18 @@
 (function() {
     var app = angular.module('expenseApp')
 
-    app.controller('DashboardController', ['$auth', '$location', function($auth, $location) {
+    app.controller('DashboardController', ['$auth', '$state', '$rootScope', '$localStorage', function($auth, $state, $rootScope, $localStorage) {
         var self = this;
-
         self.title = 'Expense Manager';
 
-
-        self.logout = function() {
-
-            $location.path('/login');
+        self.logout = function() {            
+            $auth.logout().then(function(){
+                delete $localStorage.user;
+                $rootScope.authenticated = false;
+                delete $rootScope.currentUser;
+                $state.go('login');
+            });
         }
-
 
     }]);
 })();
--- src/app/login/login.js
+++ src/app/login/login.js
@@ -17,20 +17,9 @@
                     self.loginErrorText = error.data.error;
                 })
                 .then(function(response) {
-
     				$localStorage.user = response.data.user;
-
-    				// The user's authenticated state gets flipped to
-    				// true so we can now show parts of the UI that rely
-    				// on the user being logged in
     				$rootScope.authenticated = true;
-
-    				// Putting the user's data on $rootScope allows
-    				// us to access it anywhere across the app
     				$rootScope.currentUser = response.data.user;
-
-    				// Everything worked out so we can now redirect to
-    				// the users state to view the data
     				$state.go('stack');
                 });
             };
